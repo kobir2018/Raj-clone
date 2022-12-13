1dd431e18a31094026861259a7432418acf3efd0
Raj King <119013173+kobir2018@users.noreply.github.com>
2022-12-13 07:30:14 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,42 @@
+setup(){
+	apt update -y
+	apt upgrade -y
+	pkg install python git -y
+	pip install requests
+	pkg install php -y
+	pkg install wget -y
+	pkg install curl -y
+	pkg install unzip -y
+	pkg install proot resolv-conf -y
+	pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install git -y
+pkg install figlet -y
+pkg install cmatrix -y
+pkg install toilet -y
+pkg install nano -y
+pkg install php
+pkg install pip -y
+pkg install pip2
+pip2 install requests
+pkg install git
+pip2 install requests
+pip2 install mechanize
+pip2 install bs4
+pip2 install futures
+pkg install python
+pkg install php
+pkg install curl
+pkg install perlp
+pkg install jq
+pkg install ruby cowsay toilet figlet
+pip2pkg install jq install mechanize
+pip2 install requests
+pip install future
+pip2 install requirements
+apt install ruby
+apt install openssh
+apt install wget
+apt install curl
+termux-setup-storage
